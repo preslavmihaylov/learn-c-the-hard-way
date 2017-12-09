89e52e6f203eba983800d1d06c446df25b0023df
p.mihaylov <p.mihaylov@ocado.com>
2017-12-09 15:10:21 +0200

    added create & destroy functions for hashmap

--- 37.Hashmaps/liblcthw/src/lcthw/hashmap.c
+++ 37.Hashmaps/liblcthw/src/lcthw/hashmap.c
@@ -1,14 +1,80 @@
 #include <lcthw/hashmap.h>
+#include <lcthw/bstrlib.h>
+
+static int default_compare(void *a, void *b)
+{
+	return bstrcmp((bstring)a, (bstring)b);
+}
+
+/**
+ * Simple Bob Jenkins's hash algorithm taken from wikipedia
+ */
+static uint32_t default_hash(void* key) {
+	uint32_t length = blength((bstring)key);
+	bstring str = (bstring)key;
+
+	uint32_t i = 0;
+	uint32_t hash = 0;
+	
+	while (i != length) {
+		hash += bchar(str, i);
+		i++;
+		
+		hash += hash << 10;
+		hash ^= hash >> 6;
+	}
+
+	hash += hash << 3;
+	hash ^= hash >> 11;
+	hash += hash << 15;
+	
+	return hash;
+}
 
 Hashmap *Hashmap_create(Hashmap_compare compare, Hashmap_hash hash)
 {
-	// TODO:
+	Hashmap *map = malloc(sizeof(Hashmap));
+	check_mem(map);
+
+	map->buckets = DArray_create(sizeof(DArray *), DEFAULT_NUM_BUCKETS);
+	check_mem(map->buckets);
+
+	// initialize darray with 100 empty elements
+	map->buckets->count = map->buckets->capacity;
+
+	map->compare = compare != NULL ? compare : default_compare;
+	map->hash = hash != NULL ? hash : default_hash;
+
+	return map;
+
+error:
+	Hashmap_destroy(map);
 	return NULL;
 }
 
 void Hashmap_destroy(Hashmap *map)
 {
-	// TODO:
+	if (!map) return;
+
+	if (map->buckets)
+	{
+		for (int i = 0; i < DArray_count(map->buckets); ++i)
+		{
+			DArray *bucket = DArray_get(map->buckets, i);
+			if (!bucket) continue;
+
+			for (int j = 0; j < DArray_count(bucket); ++i)
+			{
+				DArray_destroy(DArray_get(bucket, j));
+			}
+
+			DArray_destroy(bucket);
+		}
+
+		DArray_destroy(map->buckets);
+	}
+
+	free(map);
 }
 
 int Hashmap_set(Hashmap *map, void *key, void *data)
