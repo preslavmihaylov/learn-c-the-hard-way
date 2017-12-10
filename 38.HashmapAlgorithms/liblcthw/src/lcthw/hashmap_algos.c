aa546ebadfae7c818f61433abb59ee1239487f6f
p.mihaylov <p.mihaylov@ocado.com>
2017-12-10 13:35:34 +0200

    implemented fnv1a hash in hashmap_algos

--- 38.HashmapAlgorithms/liblcthw/src/lcthw/hashmap_algos.c
+++ 38.HashmapAlgorithms/liblcthw/src/lcthw/hashmap_algos.c
@@ -1,9 +1,21 @@
 #include <lcthw/hashmap_algos.h>
+#include <lcthw/bstrlib.h>
+
+#define FNV_OFFSET_BASIS 2166136261
+#define FNV_PRIME 16777619
 
 uint32_t Hashmap_fnv1a_hash(void *data)
 {
-	// TODO:
-	return 0;
+	bstring str = (bstring)data;
+	uint32_t hash = FNV_OFFSET_BASIS;
+
+	for (int i = 0; i < blength(str); ++i)
+	{
+		hash = hash ^ bchar(str, i);
+		hash = hash * FNV_PRIME;
+	}
+
+	return hash;
 }
 
 uint32_t Hashmap_adler32_hash(void *data)
