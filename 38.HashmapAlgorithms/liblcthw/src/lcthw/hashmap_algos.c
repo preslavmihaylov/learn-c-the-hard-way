49b1d24adbd91da9a3f6dd341611a9cf823a1e8d
p.mihaylov <p.mihaylov@ocado.com>
2017-12-10 13:14:58 +0200

    added hashmap_algos skeleton

---
+++ 38.HashmapAlgorithms/liblcthw/src/lcthw/hashmap_algos.c
@@ -0,0 +1,19 @@
+#include <lcthw/hashmap_algos.h>
+
+uint32_t Hashmap_fnv1a_hash(void *data)
+{
+	// TODO:
+	return 0;
+}
+
+uint32_t Hashmap_adler32_hash(void *data)
+{
+	// TODO:
+	return 0;
+}
+
+uint32_t Hashmap_djb_hash(void *data)
+{
+	// TODO:
+	return 0;
+}
