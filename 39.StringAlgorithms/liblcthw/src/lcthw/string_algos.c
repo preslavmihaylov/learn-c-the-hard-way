0eca14172d793336557db7675b8e6cfa35ecc0b6
p.mihaylov <p.mihaylov@ocado.com>
2017-12-13 17:48:41 +0200

    implemented skip_chars building in string_algos

--- 39.StringAlgorithms/liblcthw/src/lcthw/string_algos.c
+++ 39.StringAlgorithms/liblcthw/src/lcthw/string_algos.c
@@ -1,5 +1,21 @@
 #include <lcthw/string_algos.h>
 
+static inline void String_setup_skip_chars(
+	size_t *skip_chars, const unsigned char *searchedTerm, ssize_t length)
+{
+	int i;
+
+	for (i = 0; i < UCHAR_MAX + 1; i++)
+	{
+		skip_chars[i] = length;
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		skip_chars[searchedTerm[i]] = length - i - 1;
+	}
+}
+
 StringScanner *StringScanner_create(bstring in)
 {
 	// TODO:
