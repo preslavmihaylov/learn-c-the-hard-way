c0d367ac33adf18d2ac35aa9404fbc3f3afd7234
PreslavMihaylov <preslav@pmihaylov.com>
2017-12-17 23:43:51 +0200

    reformatted static functions in string_algos

--- 39.StringAlgorithms/liblcthw/src/lcthw/string_algos.c
+++ 39.StringAlgorithms/liblcthw/src/lcthw/string_algos.c
@@ -1,6 +1,49 @@
 #include <lcthw/string_algos.h>
 
 static inline void String_setup_skip_chars(
+	size_t *skipChars, const unsigned char *term, ssize_t length);
+
+static inline const unsigned char *String_base_search(
+	const unsigned char *text, ssize_t textLength,
+	const unsigned char *term, ssize_t termLength,
+	size_t *skipChars);
+
+int String_find(bstring in, bstring what)
+{
+	const unsigned char *text = (const unsigned char *)bdata(in);
+	ssize_t textLength = blength(in);
+
+	const unsigned char *term = (const unsigned char *)bdata(what);
+	ssize_t termLength = blength(what);
+
+	size_t skipChars[UCHAR_MAX + 1];
+
+	String_setup_skip_chars(skipChars, term, termLength);
+	const unsigned char *found =
+		String_base_search(text, textLength, term, termLength, skipChars);
+
+	return found != NULL ? found - text : -1;
+}
+
+StringScanner *StringScanner_create(bstring in)
+{
+	// TODO:
+	return NULL;
+}
+
+
+int StringScanner_scan(StringScanner *scan, bstring toFind)
+{
+	// TODO:
+	return 0;
+}
+
+void StringScanner_destroy(StringScanner *scan)
+{
+	// TODO:
+}
+
+static inline void String_setup_skip_chars(
 	size_t *skipChars, const unsigned char *term, ssize_t length)
 {
 	int i;
@@ -41,38 +84,3 @@ static inline const unsigned char *String_base_search(
 
 	return NULL;
 }
-
-StringScanner *StringScanner_create(bstring in)
-{
-	// TODO:
-	return NULL;
-}
-
-int String_find(bstring in, bstring what)
-{
-	const unsigned char *text = (const unsigned char *)bdata(in);
-	ssize_t textLength = blength(in);
-
-	const unsigned char *term = (const unsigned char *)bdata(what);
-	ssize_t termLength = blength(what);
-
-	size_t skipChars[UCHAR_MAX + 1];
-
-	String_setup_skip_chars(skipChars, term, termLength);
-	const unsigned char *found =
-		String_base_search(text, textLength, term, termLength, skipChars);
-
-	return found != NULL ? found - text : -1;
-}
-
-int StringScanner_scan(StringScanner *scan, bstring toFind)
-{
-	// TODO:
-	return 0;
-}
-
-void StringScanner_destroy(StringScanner *scan)
-{
-	// TODO:
-}
-
