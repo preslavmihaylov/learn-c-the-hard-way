0caad12313eae81dc7796c9a8f323ca811f0a485
PreslavMihaylov <preslav@pmihaylov.com>
2017-12-17 23:45:41 +0200

    renamed some fields of StringScanner

--- 39.StringAlgorithms/liblcthw/src/lcthw/string_algos.h
+++ 39.StringAlgorithms/liblcthw/src/lcthw/string_algos.h
@@ -7,16 +7,16 @@
 #include <stdbool.h>
 
 typedef struct StringScanner {
-	bstring in;
-	const unsigned char *haystack;
-	ssize_t hlen;
-	const unsigned char *needle;
-	ssize_t nlen;
+	bstring input;
+	const unsigned char *text;
+	ssize_t textLength;
+	const unsigned char *term;
+	ssize_t termLength;
 	size_t skip_chars[UCHAR_MAX + 1];
 } StringScanner;
 
-StringScanner *StringScanner_create(bstring in);
 int String_find(bstring in, bstring what);
+StringScanner *StringScanner_create(bstring in);
 int StringScanner_scan(StringScanner *scan, bstring toFind);
 void StringScanner_destroy(StringScanner *scan);
 
