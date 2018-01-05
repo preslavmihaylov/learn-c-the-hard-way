2aef0c5272084c247d163f71c37954ef0575f2b3
PreslavMihaylov <preslav@pmihaylov.com>
2018-01-05 17:28:31 +0200

    created templates for shell commands

--- 41.ProjectDevPkg/devpkg/src/shell.c
+++ 41.ProjectDevPkg/devpkg/src/shell.c
@@ -12,36 +12,51 @@ int Shell_exec(Shell cmd, ...)
 
 Shell CLEANUP_SH =
 {
-
+	.dir = "/tmp",
+	.exe = "rm",
+	.args = { "rm", "-rf", "/tmp/pkg-build", "/tmp/pkg-src.tar.gz",
+		"/tmp/pkg-src.tar.bz2", "/tmp/DEPENDS", NULL }
 };
 
 Shell GIT_SH =
 {
-
+	.dir = "/tmp",
+	.exe = "git",
+	.args = { "git", "clone", "URL", "pkg-build", NULL }
 };
 
 Shell TAR_SH =
 {
-
+	.dir = "/tmp/pkg-build",
+	.exe = "tar",
+	.args = { "tar", "-xzf", "FILE", "--strip-components", "1", NULL }
 };
 
 Shell CURL_SH =
 {
-
+	.dir = "/tmp",
+	.exe = "curl",
+	.args = { "curl", "-L", "-o", "TARGET", "URL", NULL }
 };
 
 Shell CONFIGURE_SH =
 {
-
+	.dir = "/tmp/pkg-build",
+	.exe = "./configure",
+	.args = { "configure", "OPTS", NULL }
 };
 
 Shell MAKE_SH =
 {
-
+	.dir = "/tmp/pkg-build",
+	.exe = "make",
+	.args = { "make", "OPTS", NULL }
 };
 
 Shell INSTALL_SH =
 {
-
+	.dir = "/tmp/pkg-build",
+	.exe = "sudo",
+	.args = { "sudo", "make", "TARGET", NULL }
 };
 
