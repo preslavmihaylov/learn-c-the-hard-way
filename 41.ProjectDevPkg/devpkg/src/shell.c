c452aaa751ee89eff7f06839f2642528f273ca35
PreslavMihaylov <preslav@pmihaylov.com>
2018-01-05 17:44:42 +0200

    implemented Shell_run

--- 41.ProjectDevPkg/devpkg/src/shell.c
+++ 41.ProjectDevPkg/devpkg/src/shell.c
@@ -1,8 +1,35 @@
 #include <shell.h>
+#include <dbg.h>
 
 int Shell_run(apr_pool_t *p, Shell *cmd)
 {
+	apr_procattr_t *attr;
+	apr_status_t exitStatus;
+
+	exitStatus = apr_procattr_create(&attr, p);
+	check(exitStatus == APR_SUCCESS, "Failed to initialize proc attributes");
+
+	exitStatus = apr_procattr_cmdtype_set(attr, APR_PROGRAM_PATH);
+	check(exitStatus == APR_SUCCESS, "Failed to set cmd type");
+
+	exitStatus = apr_procattr_dir_set(attr, cmd->dir);
+	check(exitStatus == APR_SUCCESS, "Failed to set cmd directory");
+
+	exitStatus = apr_procattr_io_set(attr, APR_NO_PIPE, APR_NO_PIPE, APR_NO_PIPE);
+	check(exitStatus == APR_SUCCESS, "Failed to set cmd file descriptors");
+
+	exitStatus = apr_proc_create(&cmd->proc, cmd->exe, cmd->args, NULL, attr, p);
+	check(exitStatus == APR_SUCCESS, "Failed to create process");
+
+	exitStatus = apr_proc_wait(&cmd->proc, &cmd->exit_code, &cmd->exit_why, APR_WAIT);
+	check(exitStatus == APR_CHILD_DONE, "Process child didn't exit normally");
+	check(cmd->exit_code == 0, "Process exit code not 0, Actual: %d", cmd->exit_code);
+	check(cmd->exit_why == APR_PROC_EXIT, "Process didn't exit normally");
+
 	return 0;
+
+error:
+	return -1;
 }
 
 int Shell_exec(Shell cmd, ...)
