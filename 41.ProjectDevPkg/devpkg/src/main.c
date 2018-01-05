7f128e30a2ab082a340f41ba7d80d70e356cfcc0
PreslavMihaylov <preslav@pmihaylov.com>
2018-01-05 18:14:50 +0200

    test shell functions in main

--- 41.ProjectDevPkg/devpkg/src/main.c
+++ 41.ProjectDevPkg/devpkg/src/main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <bstrlib.h>
 #include <db.h>
+#include <dbg.h>
 #include <shell.h>
 #include <apr_general.h>
 
 int main(int argc, char *argv[])
 {
 	apr_initialize();
-	DB_init();
 
-	DB_update("Hello world");
-	bool res1 = DB_find("Hello world");
-	bool res2 = DB_find("Hello worl");
-	bool res3 = DB_find("world");
-	printf("Hello world found? %d\n", res1);
-	printf("Hello worl found? %d\n", res2);
-	printf("world found? %d\n", res3);
+	Shell_exec(GIT_SH, "URL", "https://github.com/PreslavMihaylov/dotfiles", NULL);
 
-	DB_list();
 	apr_terminate();
 }
