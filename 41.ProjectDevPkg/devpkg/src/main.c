50d85a6ca9f116999173abedbf5e019e60495c1c
p.mihaylov <p.mihaylov@ocado.com>
2018-01-11 17:03:21 +0200

    finished devpkg

--- 41.ProjectDevPkg/devpkg/src/main.c
+++ 41.ProjectDevPkg/devpkg/src/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <dbg.h>
 #include <commands.h>
+#include <db.h>
 #include <apr_general.h>
 #include <apr_getopt.h>
 
@@ -10,6 +11,9 @@
 static void initCmdArgs(apr_getopt_t **options,
     int argc, const char * const * argv);
 static void parseCmdArgs(apr_getopt_t *options);
+static void executeCmd(CommandType cmdType,
+    const char *url, const char *configOpts, const char *makeOpts,
+    const char *installOpts);
 
 int main(int argc, const char * const * argv)
 {
@@ -36,39 +40,61 @@ static void initCmdArgs(apr_getopt_t **options,
 static void parseCmdArgs(apr_getopt_t *options)
 {
     char opt;
-    const char * optArgument;
+    const char *optArgument;
+    char url[MAX_OPTS_LEN];
     char configOpts[MAX_OPTS_LEN] = { 0 };
     char makeOpts[MAX_OPTS_LEN] = { 0 };
     char installOpts[MAX_OPTS_LEN] = { 0 };
-    CommandType cmdType;
+    CommandType cmdType = COMMAND_NONE;
 
-    while (apr_getopt(options, "I:Lc:m:i:d:SF:B:", &opt, &optArgument) == APR_SUCCESS)
+    while (apr_getopt(options, "I:Lc:m:i:SF:B:", &opt, &optArgument) == APR_SUCCESS)
     {
         switch (opt)
         {
             case 'I':
             {
+                check(cmdType == COMMAND_NONE,
+                    "Cannot specify more than one command type");
                 cmdType = COMMAND_INSTALL;
+                check(url != NULL, "Install command expects an argument");
+                strncpy(url, optArgument, MAX_OPTS_LEN);
+
                 break;
             }
             case 'L':
             {
+                check(cmdType == COMMAND_NONE,
+                    "Cannot specify more than one command type");
                 cmdType = COMMAND_LIST;
+
                 break;
             }
             case 'S':
             {
+                check(cmdType == COMMAND_NONE,
+                    "Cannot specify more than one command type");
                 cmdType = COMMAND_INIT;
+
                 break;
             }
             case 'F':
             {
+                check(cmdType == COMMAND_NONE,
+                    "Cannot specify more than one command type");
+                check(url != NULL, "Fetch command expects an argument");
                 cmdType = COMMAND_FETCH;
+                strncpy(url, optArgument, MAX_OPTS_LEN);
+
                 break;
             }
             case 'B':
             {
+                check(cmdType == COMMAND_NONE,
+                    "Cannot specify more than one command type");
+                check(url != NULL, "Build command expects an argument");
                 cmdType = COMMAND_BUILD;
+                strncpy(url, optArgument, MAX_OPTS_LEN);
+
                 break;
             }
             case 'c':
@@ -86,16 +112,66 @@ static void parseCmdArgs(apr_getopt_t *options)
                 strncpy(installOpts, optArgument, MAX_OPTS_LEN);
                 break;
             }
-            case 'd':
-            {
-                break;
-            }
         }
-
-        log_info("CMD Type: %d", cmdType);
-        log_info("Config opts: %s", configOpts);
-        log_info("Make opts: %s", makeOpts);
-        log_info("Install opts: %s", installOpts);
     }
+
+    check(cmdType != COMMAND_NONE, "No command specified.");
+    executeCmd(cmdType, url, configOpts, makeOpts, installOpts);
+
+error: // fallthrough
+    return;
 }
 
+static void executeCmd(CommandType cmdType,
+    const char *url, const char *configOpts, const char *makeOpts,
+    const char *installOpts)
+{
+    int rc = 0;
+
+    switch (cmdType)
+    {
+        case COMMAND_INSTALL:
+        {
+            rc = Command_install(url, configOpts, makeOpts, installOpts);
+            check(rc == 0, "Failed to install %s", url);
+
+            break;
+        }
+        case COMMAND_LIST:
+        {
+            int rc = DB_list();
+            check(rc == 0, "Failed to list database contents");
+
+            break;
+        }
+        case COMMAND_FETCH:
+        {
+            rc = Command_fetch(url);
+            check(rc == 0, "Failed to fetch %s", url);
+            log_info("Downloaded to %s and in /tmp/", BUILD_DIR);
+
+            break;
+        }
+        case COMMAND_INIT:
+        {
+            rc = DB_init();
+            check(rc == 0, "Failed to create devpkg database");
+
+            break;
+        }
+        case COMMAND_BUILD:
+        {
+            rc = Command_build(url, configOpts, makeOpts, installOpts);
+            check(rc == 0, "Failed to build %s", url);
+
+            break;
+        }
+        default:
+        {
+            sentinel("Invalid command given");
+        }
+    }
+
+error: // fallthrough
+    return;
+}
