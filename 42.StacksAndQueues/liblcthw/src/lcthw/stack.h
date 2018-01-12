f68b1474b7cfba69f5657a937074127e822319bc
p.mihaylov <p.mihaylov@ocado.com>
2018-01-12 17:49:41 +0200

    created the stack for ex 42

---
+++ 42.StacksAndQueues/liblcthw/src/lcthw/stack.h
@@ -0,0 +1,17 @@
+#ifndef LCTHW_STACK
+#define LCTHW_STACK
+
+#include <lcthw/list.h>
+
+typedef List Stack;
+
+#define Stack_create List_create
+#define Stack_destroy List_destroy
+#define Stack_push List_push
+#define Stack_pop List_pop
+#define Stack_peek List_last
+#define Stack_count List_count
+
+#define STACK_FOREACH(curr, stack) LIST_FOREACH(curr, stack)
+
+#endif // LCTHW_STACK
