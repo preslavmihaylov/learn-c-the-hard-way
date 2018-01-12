4dae4162f3f394e9c2ee06036c851a9561ccf011
p.mihaylov <p.mihaylov@ocado.com>
2018-01-12 17:51:39 +0200

    added some more utilities to the stack

--- 42.StacksAndQueues/liblcthw/src/lcthw/stack.h
+++ 42.StacksAndQueues/liblcthw/src/lcthw/stack.h
@@ -7,9 +7,13 @@ typedef List Stack;
 
 #define Stack_create List_create
 #define Stack_destroy List_destroy
+#define Stack_clear List_clear
+#define Stack_clear_destroy List_clear_destroy
+
 #define Stack_push List_push
 #define Stack_pop List_pop
 #define Stack_peek List_last
+
 #define Stack_count List_count
 
 #define STACK_FOREACH(curr, stack) LIST_FOREACH(curr, stack)
