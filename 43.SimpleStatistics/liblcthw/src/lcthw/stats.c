a313244be469b328392a15718653eb313fa30e24
p.mihaylov <p.mihaylov@ocado.com>
2018-01-16 17:39:45 +0200

    created the initial files for stats

---
+++ 43.SimpleStatistics/liblcthw/src/lcthw/stats.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <lcthw/stats.h>
+
+Stats *Stats_recreate(double sum, double sumsq, uint32_t count, double min, double max)
+{
+    return NULL;
+}
+
+Stats *Stats_create()
+{
+    return NULL;
+}
+
+
+double Stats_mean(Stats *stats)
+{
+    return 0;
+}
+
+double Stats_stddev(Stats *stats)
+{
+    return 0;
+}
+
+
+void Stats_sample(Stats *stats, double sample)
+{
+    return;
+}
+
+void Stats_dump(Stats *stats)
+{
+    return;
+}
+
